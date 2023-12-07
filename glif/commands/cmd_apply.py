0e16aefd86fe39e21c2af8df68952483c03ee9e1
jfschaefer <jan.frederik.schaefer@fau.de>
2023-12-07 13:58:11 +0100

    extend apply command to acting on all items at once

--- glif/commands/cmd_apply.py
+++ glif/commands/cmd_apply.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 from ..glif_abc import GlifABC as Glif
-from glif.commands.items import Items, Repr
+from glif.commands.items import Items, Repr, Item
 from .glif_command import GlifCommandType, GlifArg
 from ..elpi import runelpi, items_to_stdin
 
@@ -19,18 +19,28 @@ def apply_helper(glif: Glif, keyval: dict[str, str], keys: set[str], mainargs: l
         file += '.elpi'
     predicate = keyval['predicate']
     new_items = Items([])
-    new_items.errors = items.errors
-    for item in items.items:
-        stdin = items_to_stdin(Items([item]), with_ast)
-        r = runelpi(glif.get_cwd(), file, f'glif.apply_to_item {predicate}', typecheck, stdin)
+    if 'together' in keys:
+        stdin = items_to_stdin(items, with_ast)
+        r = runelpi(glif.get_cwd(), file, f'glif.apply_to_items {predicate}', typecheck, stdin)
         if not r.success:
-            return items.with_errors(items.errors + [r.logs])
-        if not r.success:
-            new_items.errors.append(r.logs)
-            continue
+            return Items([]).with_errors(items.errors + [r.logs])
         assert r.value
-        new_item = item.get_clone().with_repr(Repr.DEFAULT, r.value[0])
+        new_item = Item(0).with_repr(Repr.DEFAULT, r.value[0])
         new_items.items.append(new_item)
+    else:
+        new_items.errors = items.errors
+        for item in items.items:
+            stdin = items_to_stdin(Items([item]), with_ast)
+            r = runelpi(glif.get_cwd(), file, f'glif.apply_to_item {predicate}', typecheck, stdin)
+            if not r.success:
+                return items.with_errors(items.errors + [r.logs])
+            # if not r.success:
+            #     new_items.errors.append(r.logs)
+            #     continue
+            assert r.value
+            new_item = item.get_clone().with_repr(Repr.DEFAULT, r.value[0])
+            new_items.items.append(new_item)
+
     return new_items
 
 
@@ -41,6 +51,7 @@ APPLY_COMMAND_TYPE = GlifCommandType(
         GlifArg(names=['file', 'f'], description='Elpi file', default_value='$DEFAULT'),
         GlifArg(names=['predicate', 'p'], description='Predicate to be applied', default_value='apply'),
         GlifArg(names=['with-AST', 'wA'], description='Include ASTs if available'),
+        GlifArg(names=['together', 't'], description='Pass all items at once to the predicate (as as a list)'),
     ],
     description='Applies an ELPI predicate to logical expressions and returns the output',
     apply_fn=apply_helper,
